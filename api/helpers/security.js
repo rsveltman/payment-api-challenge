8aa3d52580e112c659f24e374ca9e0e9d1614955
RSV <rsoveltman@gmail.com>
2018-04-02 02:00:16 +0000

    Changed api key to environment variable

--- api/helpers/security.js
+++ api/helpers/security.js
@@ -1,7 +1,7 @@
 module.exports = {
     apiKeyAuth: function securityHandler(req, authOrSecDef, scopesOrApiKey, cb) {
         // check token
-        if (req.headers['x-api-key'] === '1234'){ // put this in an ENV VAR
+        if (req.headers['x-api-key'] === process.env.API_KEY){
           cb(null);
         } else {
           var err = new Error('Access denied - wrong token');
